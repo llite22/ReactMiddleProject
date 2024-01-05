4f6a32045fd9dcd89c9f30e76da8ebff7140746e
llite22 <llite2@yandex.ru>
2024-01-05 20:41:26 +0300

    add error boundary

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,17 @@ import App from "./app/App.tsx";
 import "./app/Styles/index.scss";
 import { ThemeProvider } from "./app/providers/ThemeProvider";
 import "./shared/config/i18n/i18n";
+import { ErrorBoundary } from "./app/providers/ErrorBoundary/index.ts";
+import { PageError } from "./widgets/PageError/ui/PageError.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
+      <ErrorBoundary fallback={<PageError />}>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </ErrorBoundary>
     </BrowserRouter>
   </React.StrictMode>
 );
