53ef1ad74fc0af104bb4e9ca9b2071ab5cefbde4
llite22 <llite2@yandex.ru>
2024-04-08 23:48:52 +0300

    fix

--- src/app/providers/StoreProvider/ui/StoreProvider.tsx
+++ src/app/providers/StoreProvider/ui/StoreProvider.tsx
@@ -1,16 +1,19 @@
 import { Provider } from "react-redux";
 import { createReduxStore } from "../config/store";
 import { StateSchema } from "../config/StateSchema";
+import { ReducersMapObject } from "@reduxjs/toolkit";
 
 interface StoreProviderProps {
   children?: React.ReactNode;
   initialState?: StateSchema;
+  asyncReducers?: ReducersMapObject<StateSchema>;
 }
 
 export const StoreProvider = ({
   children,
   initialState,
+  asyncReducers,
 }: StoreProviderProps) => {
-  const store = createReduxStore(initialState);
+  const store = createReduxStore(initialState, asyncReducers);
   return <Provider store={store}>{children}</Provider>;
 };
